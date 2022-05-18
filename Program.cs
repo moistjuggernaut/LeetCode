f4d6f8a2a28e95cd280abd79d9dc5f08ef5d3df3
RBA <robert.kuchera@gmail.com>
2022-05-18 20:28:25 +0200

    merge to main

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿public class Program {
+  public static void Main() {
+    Solution solution = new Solution();
+    solution.RomanToInt("III");
+    solution.RomanToInt("LVIII");
+    solution.RomanToInt("MCMXCIV");
+  }
+}
