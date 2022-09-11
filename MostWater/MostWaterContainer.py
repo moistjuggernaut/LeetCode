f1056527d24b4ba03d11416372ba7a5bdc3e4177
RBA <robert.kuchera@gmail.com>
2022-09-11 20:11:12 +0200

    Refactor, but worse solution than previous

--- MostWater/MostWaterContainer.py
+++ MostWater/MostWaterContainer.py
@@ -4,28 +4,32 @@ from typing import List
 
 
 class Solution:
-    # def maxArea(self, height: List[int]) -> int:
-    #     result = 0
-    #     for i, num in enumerate(height):
-    #         for y, inner in enumerate(height):
-    #             current = min(num, inner)*(y-i)
-    #             if current > result:
-    #                 result = current
-    #     return result
-
+    # Niet perse een betere oplossing dan de vorige commit
     def maxArea(Self, height: List[int]) -> int:
-        i, j = 0, len(height) - 1
-        result = 0
-        while i < j:
-            # for speed, min and max functions are bad
-            result = max(result, min(height[i], height[j]) * (j-i))
-            if height[i] < height[j]:
-                i += 1
+        lp, rp = 0, len(height) - 1
+        max_left = height[lp]
+        max_right = height[rp]
+        result = min(height[lp], height[rp]) * (rp-lp)
+        if len(height) == 2:
+            return min(height)
+        while lp < rp:
+            if max_left < max_right:
+                if max_left < height[lp]:
+                    max_left = height[lp]
+                    result = max(result, min(height[lp], height[rp]) * (rp-lp))
+                else:
+                    lp += 1
             else:
-                j -= 1
+                if max_right < height[rp]:
+                    max_right = height[rp]
+                    result = max(result, min(height[lp], height[rp]) * (rp-lp))
+                else:
+                    rp -= 1
         return result
 
 
 height = [1, 8, 6, 2, 5, 4, 8, 3, 7]
+height = [1, 1]
+height = [4, 3, 2, 1, 4]
 sln = Solution()
 sln.maxArea(height)
